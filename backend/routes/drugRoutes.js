ae52431799561fcc44d08a5978ef269ebc9bf7b7
SugavaneshwaranP <sugavanesh08@gmail.com>
2025-07-15 16:16:35 +0530

    changed

--- backend/routes/drugRoutes.js
+++ backend/routes/drugRoutes.js
@@ -14,7 +14,7 @@ router.post("/", async (req, res) => {
     const savedDrug = await newDrug.save();
     res.status(201).json(savedDrug);
   } catch (error) {
-    res.status(500).json({ message: "Failed to add drug", error });
+    res.status(500).json({ message: "Failed to add ", error });
   }
 });
 
